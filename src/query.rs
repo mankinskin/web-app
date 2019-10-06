bc9fc7cd9d74c46d06b3ee1413b8fa1df01ca3f8
Linus Behrbohm <linusbehrbohm@web.de>
2019-10-06 13:14:26 +0200

    Make Budget and transaction method's arguments more permissive
    Using abstract Into<T> types instead of T

--- src/query.rs
+++ src/query.rs
@@ -30,25 +30,27 @@ impl<'a, C: Currency> Query<'a, C> {
             .collect()
             )
     }
-    pub fn with_partner(self, a: Person) -> Self {
-        self.filter(|t|
+    pub fn with_partner<P: Into<Person> + Clone>(self, partner: P) -> Self {
+        self.filter(move |t|
                     if let Some(p) = &t.partner {
-                        p.clone() == a
+                        p.clone() == partner.clone().into()
                     } else { false })
     }
-    pub fn with_any_partners(self, parts: Vec<Person>) -> Self {
-        self.filter(|t|
+    pub fn with_any_partners<P: Into<Person> + Clone>(self, parts: Vec<P>) -> Self {
+        let parts: Vec<Person> = parts.iter().map(|p| p.clone().into()).collect();
+        self.filter(move |t|
                     if let Some(p) = &t.partner {
                         parts.contains(p)
                     } else { false })
     }
-    pub fn with_purpose(self, purp: Purpose) -> Self {
-        self.filter(|t|
+    pub fn with_purpose<P: Into<Purpose> + Clone>(self, purp: P) -> Self {
+        self.filter(move |t|
                     t.purposes
                     .clone()
-                    .map(|ps| ps.contains(&purp)).unwrap_or(false))
+                    .map(|ps| ps.contains(&purp.clone().into())).unwrap_or(false))
     }
-    pub fn with_any_purposes(self, purps: Vec<Purpose>) -> Self {
+    pub fn with_any_purposes<P: Into<Purpose> + Clone>(self, purps: Vec<P>) -> Self {
+        let purps: Vec<Purpose> = purps.iter().map(|p| p.clone().into()).collect();
         self.filter(|t|
                     t.purposes
                     .clone()
@@ -58,7 +60,8 @@ impl<'a, C: Currency> Query<'a, C> {
                                |acc, x| acc || x))
                     .unwrap_or(false))
     }
-    pub fn with_all_purposes(self, purps: Vec<Purpose>) -> Self {
+    pub fn with_all_purposes<P: Into<Purpose> + Clone>(self, purps: Vec<P>) -> Self {
+        let purps: Vec<Purpose> = purps.iter().map(|p| p.clone().into()).collect();
         self.filter(|t|
                     t.purposes
                     .clone()
@@ -82,16 +85,22 @@ impl<'a, C: Currency> Query<'a, C> {
     }
     pub fn before(self,
                   time: DateTime<Utc>) -> Self {
-        self.filter(|t| t.date < time)
+        self.filter(|t| t.date
+                         .map(|d| d < time)
+                         .unwrap_or(false))
     }
     pub fn after(self,
                  time: DateTime<Utc>) -> Self {
-        self.filter(|t| t.date >= time)
+        self.filter(|t| t.date
+                         .map(|d| d >= time)
+                         .unwrap_or(false))
     }
     pub fn within_timespan(self,
                            start: DateTime<Utc>,
                            end: DateTime<Utc>) -> Self {
-        self.filter(|t| t.date >= start && t.date <= end)
+        self.filter(|t| t.date
+                         .map(|d| d >= start && d <= end)
+                         .unwrap_or(false))
     }
 }
 #[cfg(test)]
@@ -107,54 +116,54 @@ mod tests {
         assert!(budget.balance == Euro(140));
         assert!(budget.find().earnings().len() == 0);
         assert!(budget.find().expenses().len() == 0);
-        assert!(budget.find().with_partner("Papa".into()).len() == 0);
-        assert!(budget.find().with_purpose("Fahrstunde".into()).len() == 0);
-        assert!(budget.find().with_purpose("Arbeit".into()).len() == 0);
+        assert!(budget.find().with_partner("Papa").len() == 0);
+        assert!(budget.find().with_purpose("Fahrstunde").len() == 0);
+        assert!(budget.find().with_purpose("Arbeit").len() == 0);
 
-        budget.get(Euro(19)).set_partner("Papa".into());
+        budget.get(Euro(19)).set_partner("Papa");
         assert!(budget.balance == Euro(140+19));
         assert!(budget.find().earnings().len() == 1);
         assert!(budget.find().expenses().len() == 0);
-        assert!(budget.find().with_partner("Papa".into()).len() == 1);
-        assert!(budget.find().with_purpose("Fahrstunde".into()).len() == 0);
-        assert!(budget.find().with_purpose("Arbeit".into()).len() == 0);
-        assert!(budget.find().with_any_partners(vec!["Papa".into(), "Schölermann".into()]).len() == 1);
-        assert!(budget.find().with_any_partners(vec!["Jonas".into(), "Leon".into(), "Schölermann".into()]).len() == 0);
+        assert!(budget.find().with_partner("Papa").len() == 1);
+        assert!(budget.find().with_purpose("Fahrstunde").len() == 0);
+        assert!(budget.find().with_purpose("Arbeit").len() == 0);
+        assert!(budget.find().with_any_partners(vec!["Papa", "Schölermann"]).len() == 1);
+        assert!(budget.find().with_any_partners(vec!["Jonas", "Leon", "Schölermann"]).len() == 0);
 
-        budget.give(Euro(49)).set_purpose("Fahrstunde".into()).set_partner("Schölermann".into());
+        budget.give(Euro(49)).set_purpose("Fahrstunde").set_partner("Schölermann");
         assert!(budget.balance == Euro((140+19)-49));
         assert!(budget.find().earnings().len() == 1);
         assert!(budget.find().expenses().len() == 1);
-        assert!(budget.find().with_partner("Papa".into()).len() == 1);
-        assert!(budget.find().with_purpose("Fahrstunde".into()).len() == 1);
-        assert!(budget.find().with_purpose("Arbeit".into()).len() == 0);
-        assert!(budget.find().with_any_partners(vec!["Papa".into(), "Schölermann".into()]).len() == 2);
-        assert!(budget.find().with_any_partners(vec!["Jonas".into(), "Leon".into(), "Schölermann".into()]).len() == 1);
+        assert!(budget.find().with_partner("Papa").len() == 1);
+        assert!(budget.find().with_purpose("Fahrstunde").len() == 1);
+        assert!(budget.find().with_purpose("Arbeit").len() == 0);
+        assert!(budget.find().with_any_partners(vec!["Papa", "Schölermann"]).len() == 2);
+        assert!(budget.find().with_any_partners(vec!["Jonas", "Leon", "Schölermann"]).len() == 1);
 
-        budget.get(Euro(72)).set_purposes(vec!["Arbeit".into(), "Programmieren".into()]);
+        budget.get(Euro(72)).set_purposes(vec!["Arbeit", "Programmieren"]);
         assert!(budget.balance == Euro(((140+19)-49)+72));
         assert!(budget.find().earnings().len() == 2);
         assert!(budget.find().expenses().len() == 1);
-        assert!(budget.find().with_partner("Papa".into()).len() == 1);
-        assert!(budget.find().with_purpose("Fahrstunde".into()).len() == 1);
-        assert!(budget.find().with_purpose("Arbeit".into()).len() == 1);
-        assert!(budget.find().with_any_purposes(vec!["Programmieren".into(), "Essen".into()]).len() == 1);
-        assert!(budget.find().with_all_purposes(vec!["Programmieren".into(), "Essen".into()]).len() == 0);
-        assert!(budget.find().with_any_partners(vec!["Papa".into(), "Schölermann".into()]).len() == 2);
-        assert!(budget.find().with_any_partners(vec!["Jonas".into(), "Leon".into(), "Schölermann".into()]).len() == 1);
+        assert!(budget.find().with_partner("Papa").len() == 1);
+        assert!(budget.find().with_purpose("Fahrstunde").len() == 1);
+        assert!(budget.find().with_purpose("Arbeit").len() == 1);
+        assert!(budget.find().with_any_purposes(vec!["Programmieren", "Essen"]).len() == 1);
+        assert!(budget.find().with_all_purposes(vec!["Programmieren", "Essen"]).len() == 0);
+        assert!(budget.find().with_any_partners(vec!["Papa", "Schölermann"]).len() == 2);
+        assert!(budget.find().with_any_partners(vec!["Jonas", "Leon", "Schölermann"]).len() == 1);
 
         budget.give(Euro(19))
-              .set_purposes(vec!["Programmieren".into(), "Essen".into()])
-              .set_partner("Jonas".into());
+              .set_purposes(vec!["Programmieren", "Essen"])
+              .set_partner("Jonas");
         assert!(budget.balance == Euro((((140+19)-49)+72)-19));
         assert!(budget.find().earnings().len() == 2);
         assert!(budget.find().expenses().len() == 2);
-        assert!(budget.find().with_partner("Papa".into()).len() == 1);
-        assert!(budget.find().with_purpose("Arbeit".into()).len() == 1);
-        assert!(budget.find().with_any_purposes(vec!["Programmieren".into(), "Essen".into()]).len() == 2);
-        assert!(budget.find().with_all_purposes(vec!["Programmieren".into(), "Essen".into()]).len() == 1);
-        assert!(budget.find().with_any_partners(vec!["Papa".into(), "Schölermann".into()]).len() == 2);
-        assert!(budget.find().with_any_partners(vec!["Jonas".into(), "Leon".into(), "Schölermann".into()]).len() == 2);
+        assert!(budget.find().with_partner("Papa").len() == 1);
+        assert!(budget.find().with_purpose("Arbeit").len() == 1);
+        assert!(budget.find().with_any_purposes(vec!["Programmieren", "Essen"]).len() == 2);
+        assert!(budget.find().with_all_purposes(vec!["Programmieren", "Essen"]).len() == 1);
+        assert!(budget.find().with_any_partners(vec!["Papa", "Schölermann"]).len() == 2);
+        assert!(budget.find().with_any_partners(vec!["Jonas", "Leon", "Schölermann"]).len() == 2);
 
         budget
     }
