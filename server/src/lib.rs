7219fb0231a7ece1878d9c265d7077ec987421c2
Linus Behrbohm <linusbehrbohm@web.de>
2019-12-28 23:56:52 +0100

    Remove file logic
    it was not finished an not needed yet

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -5,7 +5,6 @@ extern crate daggy;
 extern crate nom;
 
 pub mod currency;
-pub mod file;
 pub mod budget;
 pub mod interpreter;
 pub mod transaction;
@@ -37,23 +36,6 @@ pub mod query;
 // -
 
 mod tests {
-    #[test]
-    fn new_budget_file() {
-        use crate::file::{
-            create_budget_file,
-            delete_budget_file,
-        };
-        let _budget_file = create_budget_file("test").unwrap();
-        delete_budget_file("test").unwrap()
-    }
-    #[test]
-    fn open_budget_file() {
-
-    }
-    #[test]
-    fn write_budget_file() {
-
-    }
     mod transactions {
         #[test]
         fn give() {
