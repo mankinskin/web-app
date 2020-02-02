b4772aa764655394ca09b90b6401d198afb20b7e
Linus Behrbohm <linusbehrbohm@web.de>
2020-02-02 03:05:26 +0100

    Add conditional compilation for wasm32

--- interpreter/src/lib.rs
+++ interpreter/src/lib.rs
@@ -2,58 +2,14 @@
 
 extern crate nom;
 #[macro_use] extern crate itertools;
-//extern crate linefeed;
-//extern crate termion;
 extern crate chrono;
 
+#[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
+extern crate linefeed;
+#[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
+extern crate termion;
+#[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
+pub mod shell;
+
 pub mod parse;
-//pub mod shell;
 pub mod text;
-
-use parse::*;
-
-use std::io::{
-    self,
-    Read,
-    Write,
-};
-
-fn print_help() {
-    println!("Natural language interpreter
-
-    q[uit] | exit | :q\tQuit interpreter.
-    h[elp] | ?\t\tShow help.");
-}
-//use shell::*;
-use std::sync::mpsc::{channel, Sender, Receiver};
-use std::thread::{spawn};
-
-pub fn run() -> io::Result<()> {
-    println!("Running Budget App Interpreter");
-    //let mut shell = Shell::new();
-    //shell.set_prompt("shell> ");
-
-    //loop {
-    //    print!("shell> ");
-    //    std::io::stdout().flush();
-    //    //spawn(move || {
-    //        //if let Some(key) = shell.keys().next() {
-    //        //    match key.unwrap() {
-    //        //        termion::event::Key::Up => println!("up"),
-    //        //        _ => println!("something else"),
-    //        //    }
-    //        //}
-    //        if let Some(line) = shell.lines().next() {
-    //            let line = line.unwrap();
-    //            match &line as &str {
-    //                "q" | "quit" | "exit" | ":q" => break,
-    //                "h" | "help" | "?" => print_help(),
-    //                //"history" => println!("{:?}", shell.get_history()),
-    //                line => {}//println!("{:?}", Transaction::<Euro>::parse(line)),
-    //            }
-    //            //shell.append_history_unique(line.clone());
-    //        }
-    //    //});
-    //}
-    Ok(())
-}
