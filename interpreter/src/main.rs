8dea24e4cec5e25fc8426636b2f8e20bfd7049c4
Linus Behrbohm <linusbehrbohm@web.de>
2020-02-02 03:05:26 +0100

    Add conditional compilation for wasm32

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,4 +1,3 @@
-
 // Components are things which can be combined to other components.
 // components are part of a set of components.
 // the component set defines the structure of the components value.
@@ -22,7 +21,65 @@
 // - insert a new goal
 // - define implications between states
 // - given a state, compute the implied state
+use interpreter::*;
+use parse::*;
 
+use std::io::{
+    self,
+    Write,
+};
+
+use interpreter::text::*;
+
+#[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
+use interpreter::shell::*;
+
+fn print_help() {
+    println!("Natural language interpreter
+
+    q[uit] | exit | :q\tQuit interpreter.
+    h[elp] | ?\t\tShow help.");
+}
+fn process_input(line: &str) -> io::Result<()> {
+    let input = Text::parse(line).unwrap().1;
+    println!("{:?}", input);
+    Ok(())
+}
+
+#[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
+fn run() -> io::Result<()> {
+    println!("Running Budget App Interpreter");
+    let mut shell = Shell::new();
+    shell.set_prompt("shell> ");
+
+    loop {
+        print!("shell> ");
+        std::io::stdout().flush().unwrap();
+        //spawn(move || {
+            //if let Some(key) = shell.keys().next() {
+            //    match key.unwrap() {
+            //        termion::event::Key::Up => println!("up"),
+            //        _ => println!("something else"),
+            //    }
+            //}
+            if let Some(line) = shell.lines().next() {
+                let line = line.unwrap();
+                match &line as &str {
+                    "q" | "quit" | "exit" | ":q" => break,
+                    "h" | "help" | "?" => print_help(),
+                    //"history" => println!("{:?}", shell.get_history()),
+                    line => process_input(line)?,
+                }
+                //shell.append_history_unique(line.clone());
+            }
+        //});
+    }
+    Ok(())
+}
+#[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
 fn main() {
-    interpreter::run().unwrap();
+    run().unwrap();
+}
+#[cfg(all(target_arch = "wasm32", target_os = "unknown"))]
+fn main() {
 }
