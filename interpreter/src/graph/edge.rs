5c731c5013ce124772e2c20bb9e8648cf4239196
Linus Behrbohm <linusbehrbohm@web.de>
2020-02-23 22:39:15 +0100

    Extend GraphEdge/GraphEdges

--- interpreter/src/graph/edge.rs
+++ interpreter/src/graph/edge.rs
@@ -10,10 +10,40 @@ use std::collections::{HashSet, HashMap};
 
 pub type EdgeRef<'a> = EdgeReference<'a, HashSet<usize>>;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct GraphEdge<'a>  {
-    edge: EdgeReference<'a, HashSet<usize>>,
+    edge: EdgeRef<'a>,
 }
+
+use std::fmt::{self, Display, Formatter};
+use petgraph::visit::{EdgeRef as PetgraphEdgeRef};
+impl<'a> Display for GraphEdge<'a> {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "{:?} --{:?}--> {:?}",
+               self.edge.source(),
+               self.edge.weight(),
+               self.edge.target())
+    }
+}
+impl<'a> petgraph::visit::EdgeRef for GraphEdge<'a> {
+    type NodeId = <EdgeRef<'a> as petgraph::visit::EdgeRef>::NodeId;
+    type EdgeId = <EdgeRef<'a> as petgraph::visit::EdgeRef>::EdgeId;
+    type Weight = <EdgeRef<'a> as petgraph::visit::EdgeRef>::Weight;
+    fn source(&self) -> Self::NodeId {
+        self.edge.source()
+    }
+    fn target(&self) -> Self::NodeId {
+        self.edge.target()
+    }
+    fn weight(&self) -> &Self::Weight {
+        self.edge.weight()
+    }
+    fn id(&self) -> Self::EdgeId {
+        self.edge.id()
+    }
+}
+impl<'a> Eq for GraphEdge<'a> { }
+
 impl<'a> std::ops::Deref for GraphEdge<'a> {
     type Target = EdgeRef<'a>;
     fn deref(&self) -> &Self::Target {
@@ -31,8 +61,7 @@ impl<'a> GraphEdge<'a>  {
     pub fn contains_weight(&self, w: &usize) -> bool {
         self.weight().contains(w)
     }
-    pub fn max_weight(&self) -> Option<&'a usize> {
+    pub fn max_weight(&'a self) -> Option<&'a usize> {
         self.weight().iter().max()
     }
 }
-
