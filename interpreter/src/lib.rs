c41b4b83efc34db84f475c4d7d7ae80f5f483de1
Linus Behrbohm <linusbehrbohm@web.de>
2020-02-28 15:42:02 +0100

    Add set! macro to crate root

--- interpreter/src/lib.rs
+++ interpreter/src/lib.rs
@@ -16,3 +16,18 @@ pub mod shell;
 pub mod parse;
 pub mod text;
 pub mod graph;
+
+use std::collections::{HashSet};
+
+#[macro_export]
+macro_rules! set {
+    ( $( $x:expr ),* ) => {  // Match zero or more comma delimited items
+        {
+            let mut temp_set = HashSet::new();  // Create a mutable HashSet
+            $(
+                temp_set.insert($x); // Insert each item matched into the HashSet
+            )*
+                temp_set // Return the populated HashSet
+        }
+    };
+}
