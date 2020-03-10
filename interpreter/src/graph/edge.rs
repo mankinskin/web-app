8f4bac13e12f76b8c357606ae5ab4b6d18a3f230
Linus Behrbohm <linusbehrbohm@web.de>
2020-03-10 17:45:31 +0100

    Change GraphEdge and GraphEdges implementation and add tests

--- interpreter/src/graph/edge.rs
+++ interpreter/src/graph/edge.rs
@@ -15,17 +15,40 @@ use crate::graph::*;
 
 pub type EdgeRef<'a> = EdgeReference<'a, TextGraphEdgeWeight>;
 
-#[derive(Clone, Copy, PartialEq)]
+#[derive(Clone, Copy)]
 pub struct GraphEdge<'a>  {
-    edge: EdgeRef<'a>,
+    graph: &'a TextGraph,
+    index: EdgeIndex,
 }
 
 impl<'a> GraphEdge<'a>  {
+    pub fn new(graph: &'a TextGraph, index: EdgeIndex) -> Self {
+        let r = graph.edge_references().nth(index.index()).unwrap();
+        Self {
+            graph,
+            index,
+        }
+    }
+    pub fn index(&'a self) -> &'a EdgeIndex {
+        &self.index
+    }
     pub fn contains_weight(&self, w: &usize) -> bool {
         self.weight().contains(w)
     }
     pub fn max_weight(&'a self) -> Option<&'a usize> {
         self.weight().iter().max()
+    }
+    pub fn edge_ref(&'a self) -> EdgeRef<'a> {
+        self.graph
+            .edge_references()
+            .nth(self.index.index())
+            .unwrap()
+    }
+}
+impl<'a> PartialEq for GraphEdge<'a> {
+    fn eq(&self, other: &GraphEdge<'a>) -> bool {
+        self.index == other.index &&
+            self.graph as *const _ == other.graph
     }
 }
 impl<'a> Debug for GraphEdge<'a> {
@@ -36,27 +59,14 @@ impl<'a> Debug for GraphEdge<'a> {
 impl<'a> Display for GraphEdge<'a> {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         write!(f, "{:?} --{:?}--> {:?}",
-               self.edge.source(),
-               self.edge.weight(),
-               self.edge.target())
+               self.source(),
+               self.weight(),
+               self.target())
     }
 }
 impl<'a> Hash for GraphEdge<'a> {
     fn hash<H: Hasher>(&self, h: &mut H) {
         self.id().hash(h);
-    }
-}
-impl<'a> std::ops::Deref for GraphEdge<'a> {
-    type Target = EdgeRef<'a>;
-    fn deref(&self) -> &Self::Target {
-        &self.edge
-    }
-}
-impl<'a> From<EdgeRef<'a>> for GraphEdge<'a>  {
-    fn from(edge: EdgeRef<'a>) -> Self {
-        Self {
-            edge
-        }
     }
 }
 impl<'a> Eq for GraphEdge<'a> {
@@ -66,15 +76,15 @@ impl<'a> petgraph::visit::EdgeRef for GraphEdge<'a> {
     type EdgeId = <EdgeRef<'a> as petgraph::visit::EdgeRef>::EdgeId;
     type Weight = <EdgeRef<'a> as petgraph::visit::EdgeRef>::Weight;
     fn source(&self) -> Self::NodeId {
-        self.edge.source()
+        self.edge_ref().source()
     }
     fn target(&self) -> Self::NodeId {
-        self.edge.target()
+        self.edge_ref().target()
     }
     fn weight(&self) -> &Self::Weight {
-        self.edge.weight()
+        self.edge_ref().weight()
     }
     fn id(&self) -> Self::EdgeId {
-        self.edge.id()
+        self.edge_ref().id()
     }
 }
