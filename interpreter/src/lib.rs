061ec68c69c9427c984248c2bcc93192cf3040d3
Linus Behrbohm <linusbehrbohm@web.de>
2020-03-21 15:22:43 +0100

    Allow trailing comma in set! macro, reformatting

--- interpreter/src/lib.rs
+++ interpreter/src/lib.rs
@@ -26,13 +26,13 @@ use std::collections::{HashSet};
 
 #[macro_export]
 macro_rules! set {
-    ( $( $x:expr ),* ) => {  // Match zero or more comma delimited items
+    ( $( $x:expr ),* $(,)? ) => {  // Match zero or more comma delimited items
         {
             let mut temp_set = HashSet::new();  // Create a mutable HashSet
             $(
                 temp_set.insert($x); // Insert each item matched into the HashSet
             )*
-                temp_set // Return the populated HashSet
+            temp_set // Return the populated HashSet
         }
     };
 }
