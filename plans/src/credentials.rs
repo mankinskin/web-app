3348f0b1a56f8c100dacd3a24eeaedec57d9fb81
Linus Behrbohm <linusbehrbohm@web.de>
2020-05-25 00:08:35 +0200

    Add password and username check on login page

--- plans/src/credentials.rs
+++ plans/src/credentials.rs
@@ -1,16 +1,35 @@
 use updatable::{
     *,
 };
-#[derive(Clone, Debug, Updatable)]
+#[derive(Clone, Debug, Updatable, Default)]
 pub struct Credentials {
     username: String,
     password: String,
 }
 impl Credentials {
     pub fn new() -> Self {
-        Self {
-            username: String::new(),
-            password: String::new(),
+        Self::default()
+    }
+    pub fn username_is_valid(&self) -> bool {
+        self.username_invalid_text().is_empty()
+    }
+    pub fn password_is_valid(&self) -> bool {
+        self.password_invalid_text().is_empty()
+    }
+    pub fn username_invalid_text(&self) -> String {
+        match self.username.len() {
+            0 | 8..=16 => String::new(),
+            _ => String::from(
+                "Username must be between 8 and 16 characters long."
+            )
+        }
+    }
+    pub fn password_invalid_text(&self) -> String {
+        match self.password.len() {
+            0 | 8..=16 => String::new(),
+            _ => String::from(
+                "Password must be between 8 and 16 characters long."
+            )
         }
     }
 }
