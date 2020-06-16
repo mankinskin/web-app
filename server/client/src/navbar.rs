aeaedab8673c4c86e9d981a69f9bfaffe319f8c8
Linus Behrbohm <linusbehrbohm@web.de>
2020-06-16 16:24:13 +0200

    Split App into Root and Page components

---
+++ server/client/src/navbar.rs
@@ -0,0 +1,44 @@
+use seed::{
+    *,
+    prelude::*,
+};
+
+#[derive(Clone, Default)]
+pub struct Model {
+}
+
+#[derive(Clone)]
+pub enum Msg {
+}
+
+pub fn update(_msg: Msg, _model: &mut Model, _orders: &mut impl Orders<Msg>) {
+}
+pub fn view(_model: &Model) -> Node<Msg> {
+    div![
+        div![
+            a![
+                attrs!{
+                    At::Href => "/";
+                },
+                "Home",
+            ],
+        ],
+        div![
+            a![
+                attrs!{
+                    At::Href => "/login";
+                },
+                "Login",
+            ],
+        ],
+        div![
+            a![
+                attrs!{
+                    At::Href => "/register";
+                },
+                "Register",
+
+            ],
+        ],
+    ]
+}
