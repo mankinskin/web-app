87d8ca985bb603377d6c794ce70633c359de37f1
Linus Behrbohm <linusbehrbohm@web.de>
2020-06-27 19:33:09 +0200

    Add projects view to user profile

--- server/client/src/users/profile.rs
+++ server/client/src/users/profile.rs
@@ -1,19 +1,23 @@
 use crate::{
     users::*,
+    projects,
 };
 
 #[derive(Clone)]
 pub struct Model {
     pub user: user::Model,
+    pub projects: projects::Model,
 }
 #[derive(Clone)]
 pub enum Msg {
     User(user::Msg),
+    Projects(projects::Msg),
 }
 impl From<user::Model> for Model {
     fn from(user: user::Model) -> Self {
         Self {
             user,
+            projects: projects::Model::empty(),
         }
     }
 }
@@ -26,6 +30,13 @@ pub fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg, GMsg>) 
                 &mut orders.proxy(Msg::User)
             )
         },
+        Msg::Projects(msg) => {
+            projects::update(
+                msg,
+                &mut model.projects,
+                &mut orders.proxy(Msg::Projects)
+            )
+        },
     }
 }
 pub fn view(model: &Model) -> Node<Msg> {
@@ -34,6 +45,8 @@ pub fn view(model: &Model) -> Node<Msg> {
             div![
                 h1!["Profile"],
                 p![user.name()],
+                projects::view(&model.projects)
+                    .map_msg(Msg::Projects),
             ]
         },
         None => {
