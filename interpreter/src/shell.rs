f2514756f392e0151ad919366e5bb9120d11cc71
Linus Behrbohm <linusbehrbohm@web.de>
2020-07-30 01:01:44 +0200

    Improve shell help output

--- interpreter/src/shell.rs
+++ interpreter/src/shell.rs
@@ -13,12 +13,31 @@ pub struct Shell {
     exit: bool,
 }
 
+#[derive(Clone)]
 enum Command {
     Exit,
     Help,
     Text(String),
 }
+#[macro_export]
+macro_rules! set {
+    ( $( $x:expr ),* $(,)? ) => {  // Match zero or more comma delimited items
+        {
+            let mut temp_set = std::collections::HashSet::new();  // Create a mutable HashSet
+            $(
+                temp_set.insert($x); // Insert each item matched into the HashSet
+            )*
+            temp_set // Return the populated HashSet
+        }
+    };
+}
 
+lazy_static!{
+    static ref COMMANDS: Vec<(Vec<&'static str>, Command, &'static str)> = vec![
+        (vec!["q", "quit", "exit", ":q"], Command::Exit, "Exit shell."),
+        (vec!["h", "help", "?"], Command::Help, "Show help.")
+    ];
+}
 impl Shell {
     pub fn new() -> Self {
         Self {
@@ -45,11 +64,12 @@ impl Shell {
         Ok(())
     }
     fn read_command(&self, line: &str) -> io::Result<Command> {
-        Ok(match line  {
-            "q" | "quit" | "exit" | ":q" => Command::Exit,
-            "h" | "help" | "?" => Command::Help,
-            line => Command::Text(line.to_string())
-        })
+        for (ts, cmd, _desc) in COMMANDS.iter() {
+            if ts.iter().any(|x| x == &line) {
+                return Ok(cmd.clone());
+            }
+        }
+        Ok(Command::Text(line.to_string()))
     }
     fn exec_command(&mut self, cmd: Command) -> io::Result<()> {
         Ok(match cmd {
@@ -70,9 +90,31 @@ impl Shell {
         self.stdout.flush()
     }
     fn write_help(&mut self) -> io::Result<()> {
-        write!(self.stdout, "Natural language interpreter\n
-q[uit] | exit | :q\tQuit interpreter.
-h[elp] | ?\t\tShow help.\n\n")?;
+        write!(self.stdout, "Natural language interpreter\n")?;
+        let mut lines = Vec::new();
+        let mut max = 0;
+        for (ts, _cmd, _desc) in COMMANDS.iter() {
+            let mut strs =
+                   ts.iter()
+                     .fold(String::new(),
+                     |acc, x| format!("{}{} | ", acc, x));
+            strs.pop();
+            strs.pop();
+            max = strs.len().max(max);
+            lines.push(strs);
+        }
+        for line in &mut lines {
+            let tab_width = 8;
+            let d = max - line.len();
+            let ts = (d as f32 / tab_width as f32).floor() as usize;
+            line.push_str(&std::iter::repeat("\t").take(ts).collect::<String>());
+        }
+        for (ts, desc) in lines.iter().zip(COMMANDS.iter().map(|(_, _, desc)| desc)) {
+            write!(self.stdout, "{}\t{}\n",
+                ts,
+                desc
+            )?;
+        }
         self.stdout.flush()
     }
 }
