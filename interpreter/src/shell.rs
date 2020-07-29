257932fb981ffe2d5195d30d9323f80a85b8fe7b
Linus Behrbohm <linusbehrbohm@web.de>
2020-07-30 00:33:29 +0200

    Remove history command

--- interpreter/src/shell.rs
+++ interpreter/src/shell.rs
@@ -7,20 +7,15 @@ use std::io::{
     BufRead,
 };
 
-//#[derive(Clone)]
 pub struct Shell {
     prompt: String,
-    //stdout: RawTerminal<Stdout>,
     stdout: Stdout,
-    history: Vec<String>,
-    history_index: Option<usize>,
     exit: bool,
 }
 
 enum Command {
     Exit,
     Help,
-    History,
     Text(String),
 }
 
@@ -29,8 +24,6 @@ impl Shell {
         Self {
             prompt: "> ".into(),
             stdout: stdout(),
-            history: Vec::new(),
-            history_index: None,
             exit: false,
         }
     }
@@ -40,31 +33,13 @@ impl Shell {
             self.stdout.flush()?;
             if self.exit { break }
             self.write_prompt()?;
-            //if let Some(key) = stdin().lock().keys().next() {
-            //    let key = key?;
-            //    match key {
-            //        Key::Up | Key::Down => {
-            //            match key {
-            //                Key::Up => self.inc_history_index(),
-            //                Key::Down => self.dec_history_index(),
-            //                _ => {},
-            //            }
-            //        },
-            //        _ => {},
-            //    }
-            //} else {
             if let Some(line) = stdin().lock().lines().next() {
                 let line = line?;
                 if !line.is_empty() {
                     let cmd = self.read_command(&line)?;
-                    //self.append_history_unique(line.clone());
                     self.exec_command(cmd)?
                 }
             }
-            //}
-            //if let Some(line) = self.get_history_index() {
-            //    std::io::stdout().write(line.as_bytes()).unwrap();
-            //}
             self.stdout.flush()?;
         }
         Ok(())
@@ -73,48 +48,15 @@ impl Shell {
         Ok(match line  {
             "q" | "quit" | "exit" | ":q" => Command::Exit,
             "h" | "help" | "?" => Command::Help,
-            "history" => Command::History,
             line => Command::Text(line.to_string())
         })
     }
     fn exec_command(&mut self, cmd: Command) -> io::Result<()> {
         Ok(match cmd {
             Command::Help => self.write_help()?,
-            Command::History => self.write_history()?,
             Command::Exit => { self.exit = true; },
             Command::Text(s) => self.write_line(&s)?,
         })
-    }
-    pub fn append_history_unique<S: Into<String>>(&mut self, s: S) {
-        let s = s.into();
-        if !self.history.contains(&s) {
-            self.append_history(s);
-        }
-    }
-    pub fn append_history<S: Into<String>>(&mut self, s: S) {
-        self.history.push(s.into());
-    }
-    pub fn reset_history_index(&mut self) {
-        self.history_index = None;
-    }
-    pub fn inc_history_index(&mut self) {
-        self.history_index = self.history_index.map_or(
-            Some(0),
-            |i| Some(i + 1),
-        );
-    }
-    pub fn dec_history_index(&mut self) {
-        self.history_index = self.history_index.map_or(
-            Some(0),
-            |i| Some(i - 1),
-        );
-    }
-    pub fn get_history_index(&self) -> Option<String> {
-        self.history_index
-            .and_then(|i| self.history.get(i).map(Clone::clone))
-    }
-    pub fn get_history(&self) -> Vec<String> {
-        self.history.clone()
     }
     pub fn set_prompt<S: Into<String>>(&mut self, p: S) {
         self.prompt = p.into();
@@ -131,10 +73,6 @@ impl Shell {
         write!(self.stdout, "Natural language interpreter\n
 q[uit] | exit | :q\tQuit interpreter.
 h[elp] | ?\t\tShow help.\n\n")?;
-        self.stdout.flush()
-    }
-    fn write_history(&mut self) -> io::Result<()> {
-        write!(self.stdout, "{:?}", self.history)?;
         self.stdout.flush()
     }
 }
