9bb4c31698369e56ee22d54dbaaec440892c98f8
Linus Behrbohm <linusbehrbohm@web.de>
2020-07-30 02:33:12 +0200

    Remove set! macro

--- interpreter/src/shell.rs
+++ interpreter/src/shell.rs
@@ -22,18 +22,6 @@ enum Command {
     Help,
     Text(String),
 }
-#[macro_export]
-macro_rules! set {
-    ( $( $x:expr ),* $(,)? ) => {  // Match zero or more comma delimited items
-        {
-            let mut temp_set = std::collections::HashSet::new();  // Create a mutable HashSet
-            $(
-                temp_set.insert($x); // Insert each item matched into the HashSet
-            )*
-            temp_set // Return the populated HashSet
-        }
-    };
-}
 
 lazy_static!{
     static ref COMMANDS: Vec<(Vec<&'static str>, Command, &'static str)> = vec![
