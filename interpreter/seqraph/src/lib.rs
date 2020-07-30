61746f88ee83dda6fd62def8e7f0c251a3435592
Linus Behrbohm <linusbehrbohm@web.de>
2020-07-30 13:29:19 +0200

    Uncomment join_mappings

--- interpreter/seqraph/src/lib.rs
+++ interpreter/seqraph/src/lib.rs
@@ -151,42 +151,42 @@ impl<N> SequenceGraph<N>
         })
     }
     ///// Join two EdgeMappings to a new EdgeMapping
-    //pub fn join_mappings(&self, lhs: &Mapped<N>, rhs: &Mapped<N>) -> Option<Mapped<N>> {
-    //    // TODO: make lhs and rhs contain indices
-    //    //let left_index = self.find_node_index(&lhs.data)?;
-    //    //let right_index = self.find_node_index(&rhs.data)?;
-    //    let left_outgoing = lhs.mapping.outgoing;
-    //    let right_incoming = rhs.mapping.incoming;
+    pub fn join_mappings(&self, lhs: &Mapped<N>, rhs: &Mapped<N>) -> Option<Mapped<N>> {
+        // TODO: make lhs and rhs contain indices
+        //let left_index = self.find_node_index(&lhs.data)?;
+        //let right_index = self.find_node_index(&rhs.data)?;
+        let left_outgoing = &lhs.mapping.outgoing;
+        let right_incoming = &rhs.mapping.incoming;
 
-    //    // find all edges connecting left to right with their indices
-    //    // in the matrices
-    //    let connecting_edges: Vec<(usize, usize, EdgeIndex)> = left_outgoing
-    //        .iter()
-    //        .enumerate()
-    //        .filter_map(|(li, e)| Some((li, right_incoming.iter().position(|r| r == e)?, e.clone())))
-    //        .collect();
-
-
-    //    // take left rows and right columns of matrix for connecting edges
-    //    let left_matrix = lhs.mapping.matrix;
-    //    let right_matrix = rhs.mapping.matrix;
-
-    //    //let incoming_context = left_matrix.row(left_matrix_index);
-    //    //let outgoing_context = right_matrix.column(right_matrix_index);
+        // find all edges connecting left to right with their indices
+        // in the matrices
+        let connecting_edges: Vec<(usize, usize, EdgeIndex)> = left_outgoing
+            .iter()
+            .enumerate()
+            .filter_map(|(li, e)| Some((li, right_incoming.iter().position(|r| r == e)?, e.clone())))
+            .collect();
 
 
-    //    // intersect left incoming groups i with right incoming groups i + left.width
-    //    let left_width = lhs.data.width();
-    //    let left_incoming_groups = lhs.mapping.incoming_distance_groups(&self);
-    //    let right_incoming_groups = rhs.mapping.incoming_distance_groups(&self);
+        // take left rows and right columns of matrix for connecting edges
+        let left_matrix = &lhs.mapping.matrix;
+        let right_matrix = &rhs.mapping.matrix;
 
-    //    // intersect left outgoing groups i + right.width with right outgoing groups i
-    //    let right_width = rhs.data.width();
-    //    let left_outgoing_groups = lhs.mapping.outgoing_distance_groups(&self);
-    //    let right_outgoing_groups = rhs.mapping.outgoing_distance_groups(&self);
-    //    //
-    //    Some(lhs.clone())
-    //}
+        //let incoming_context = left_matrix.row(left_matrix_index);
+        //let outgoing_context = right_matrix.column(right_matrix_index);
+
+
+        // intersect left incoming groups i with right incoming groups i + left.width
+        let left_width = lhs.data.width();
+        let left_incoming_groups = lhs.mapping.incoming_distance_groups(&self);
+        let right_incoming_groups = rhs.mapping.incoming_distance_groups(&self);
+
+        // intersect left outgoing groups i + right.width with right outgoing groups i
+        let right_width = rhs.data.width();
+        let left_outgoing_groups = lhs.mapping.outgoing_distance_groups(&self);
+        let right_outgoing_groups = rhs.mapping.outgoing_distance_groups(&self);
+        //
+        Some(lhs.clone())
+    }
 }
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct NodeInfo<N: NodeData> {
