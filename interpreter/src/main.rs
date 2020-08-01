028acd475b01c03261da795a852788574ede3f5e
Linus Behrbohm <linusbehrbohm@web.de>
2020-08-01 16:57:14 +0200

    Use regex-syntax crate to parse regex ASTs

--- interpreter/src/main.rs
+++ interpreter/src/main.rs
@@ -1,10 +1,17 @@
+extern crate regex;
+extern crate regex_syntax;
 extern crate seqraph;
+pub mod parse;
+extern crate itertools;
 #[macro_use] extern crate lazy_static;
+
+#[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
+extern crate chrono;
 #[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
 pub mod shell;
 #[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
 use shell::*;
-extern crate itertools;
+
 #[cfg(not(all(target_arch = "wasm32", target_os = "unknown")))]
 fn main() {
     let mut shell = Shell::new();
