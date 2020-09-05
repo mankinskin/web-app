28187be0db9a5f660f4589e6220e6e33b206b88d
Linus Behrbohm <linusbehrbohm@web.de>
2020-09-05 16:44:44 +0200

    Move view code in client into seperate functions

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -49,47 +49,44 @@ pub fn render() {
                View::view,
     );
 }
-#[derive(Clone, Default)]
+#[derive(Clone)]
 pub struct Model {
-    width: usize,
-    height: usize,
+    width: u32,
+    height: u32,
     data: Vec<Candle>,
     error: Option<String>,
+}
+impl Default for Model {
+    fn default() -> Self {
+        Self {
+            width: 500,
+            height: 200,
+            data: Vec::new(),
+            error: None,
+        }
+    }
 }
 #[derive(Clone, Debug)]
 pub enum Msg {
     GetHistory,
     GotHistory(Result<Vec<Candle>, String>),
 }
-async fn fetch_candles() -> Result<Vec<Candle>, FetchError> {
-    let host = "http://localhost:8000";
-    let url = format!("{}{}", host, "/api/price_history");
-    let mut req = seed::fetch::Request::new(&url)
-        .method(Method::Get);
-    seed::fetch::fetch(req)
-        .await?
-        .check_status()?
-        .json().await
-}
 impl Component for Model {
     type Msg = Msg;
     fn update(&mut self, msg: Msg, orders: &mut impl Orders<Msg>) {
         match msg {
             Msg::GetHistory => {
-                log!("Requesting Candles...");
                 orders.perform_cmd(
-                    async {
-                        fetch_candles()
-                        .await
-                        .map_err(|e| format!("{:#?}", e))
-                    }
-                    .map(Msg::GotHistory)
-                );
-                log!("Request done.");
+                        async {
+                            Self::fetch_candles()
+                                .await
+                                .map_err(|e| format!("{:#?}", e))
+                        }
+                        .map(Msg::GotHistory)
+                    );
             },
             Msg::GotHistory(res) => {
-                log!("Response:");
-                log!(res);
+                //log!(res);
                 match res {
                     Ok(candles) => self.data = candles,
                     Err(e) => self.error = Some(e),
@@ -98,27 +95,86 @@ impl Component for Model {
         }
     }
 }
-impl View for Model {
-    fn view(&self) -> Node<Self::Msg> {
-        let width = 1000;
-        let height = 400;
-        let mut rng = rand::thread_rng();
-        let points: Vec<(_, _)> =
-                if self.data.is_empty() {
-            (0..width).zip(
-                    Normal::new(height as f32/2.0, 20.0).unwrap().sample_iter(&mut rng).take(width)
-            )
-            .collect()
-                } else {
-            (0..width).zip(
-                    self.data.iter().map(|candle| candle.open.to_f32().unwrap()*1000000 as f32)
-            )
-            .collect()
-                };
+impl Model {
+    async fn fetch_candles() -> Result<Vec<Candle>, FetchError> {
+        let host = "http://localhost:8000";
+        let url = format!("{}{}", host, "/api/price_history");
+        let mut req = seed::fetch::Request::new(&url)
+            .method(Method::Get);
+        seed::fetch::fetch(req)
+            .await?
+            .check_status()?
+            .json()
+            .await
+    }
+    fn vertical_lines(&self) -> Vec<Node<<Self as Component>::Msg>> {
+        (0..(self.width/10)).fold(Vec::with_capacity(self.width as usize/10*2), |mut acc, index| {
+            let x = index*10;
+            let y = self.height as i32;
+            let h = 10;
+            let padding = 10;
 
+            let center_dir = (-1 * self.height as i32/2).clamp(-1, 1);
+            acc.push(path![
+                    attrs!{
+                        At::D => format!("M {} {} V {}", x, y, y + center_dir * h)
+                        At::Stroke => "black"
+                    }
+                ]);
+            if index % 10 == 0 {
+                acc.push(text![
+                    attrs!{
+                        At::X => x,
+                        At::Y => y + center_dir * (h + padding),
+                        At::FontFamily => "-apple-system, system-ui, BlinkMacSystemFont, Roboto",
+                        At::DominantBaseline => "middle",
+                        At::TextAnchor => "middle",
+                        At::FontSize => "9",
+                        At::Fill => "black",
+                    },
+                    index.to_string()
+                ])
+            }
+            acc
+        })
+    }
+    fn horizontal_lines(&self) -> Vec<Node<<Self as Component>::Msg>> {
+            (0..(self.height/10)).fold(Vec::with_capacity(self.height as usize/10*2), |mut acc, index| {
+                let y = index*10;
+                let x = 0;
+                let l = self.width as i32;
+                let xp = 10;
+                let yp = 10;
 
+                let center_dir = (self.width as i32/2 - x).clamp(-1, 1);
+                let emphathize = index % 10 == 0;
+                let opacity = if emphathize { 0.3 } else { 0.1 };
+                acc.push(path![
+                        attrs!{
+                            At::D => format!("M {} {} H {}", x, y, x + center_dir * l),
+                            At::Stroke => "black",
+                            At::Opacity => opacity,
+                        }
+                ]);
+                if emphathize {
+                    acc.push(text![
+                        attrs!{
+                            At::X => x + xp,
+                            At::Y => y + yp,
+                            At::FontFamily => "-apple-system, system-ui, BlinkMacSystemFont, Roboto",
+                            At::DominantBaseline => "middle",
+                            At::TextAnchor => "middle",
+                            At::FontSize => "9",
+                            At::Fill => "black",
+                        },
+                        ((self.height - y)/10).to_string()
+                    ]);
+                }
+                acc
+            })
+    }
+    fn update_button(&self) -> Node<<Self as Component>::Msg> {
         div![
-            p!["Hello from Seed!"],
             button![
                 ev(Ev::Click, |_| Msg::GetHistory),
                 "Update!"
@@ -131,121 +187,81 @@ impl View for Model {
                     e
                 ]
             } else { empty![] },
-            div![
+        ]
+    }
+    fn graph_view(&self) -> Node<<Self as Component>::Msg> {
+        let points: Vec<(_, _)> =
+                self.data
+                    .iter()
+                    .enumerate()
+                    .map(|(i, candle)|
+                        (i, candle.open
+                              .to_f32()
+                              .unwrap()*1000000 as f32)
+                    )
+                    .collect();
+        div![
+            style!{
+                St::Resize => "horizontal",
+                St::Overflow => "auto",
+                St::Height => "auto",
+            },
+            svg![
                 style!{
-                    St::Resize => "horizontal",
-                    St::Overflow => "auto",
-                    St::Height => "auto",
+                    St::BackgroundColor => "gray";
                 },
-                svg![
-                    style!{
-                        St::BackgroundColor => "gray";
-                    },
+                attrs!{
+                    At::ViewBox => format!("0 0 {} {}", self.width, self.height),
+                    At::PreserveAspectRatio => "xMinYMin meet",
+                },
+                text![
                     attrs!{
-                        At::ViewBox => format!("0 0 {} {}", width, height),
-                        At::PreserveAspectRatio => "xMinYMin meet",
+                        At::X => self.width/2,
+                        At::Y => self.height/2,
+                        At::FontFamily => "-apple-system, system-ui, BlinkMacSystemFont, Roboto",
+                        At::DominantBaseline => "middle",
+                        At::FontSize => "18",
+                        At::Fill => "black",
                     },
-                    text![
-                        attrs!{
-                            At::X => width/2,
-                            At::Y => height/2,
-                            At::FontFamily => "-apple-system, system-ui, BlinkMacSystemFont, Roboto",
-                            At::DominantBaseline => "middle",
-                            At::TextAnchor => "middle",
-                            At::FontSize => "18",
-                            At::Fill => "#74838f",
-                            At::FontWeight => "700",
-                        },
-                        "Example SVG"
-                    ],
-                    points.windows(2).map(|window| {
-                        if let [(ax, ay), (bx, by)] = window {
-                            vec![
-                                path![
-                                    attrs!{
-                                        At::D => format!("M {} {} L {} {}", ax, ay, bx, by)
-                                        At::Stroke => "black"
-                                    }
-                                ],
-                                circle![
-                                    attrs!{
-                                        At::Cx => format!("{}", bx)
-                                        At::Cy => format!("{}", by)
-                                        At::R => "1",
-                                        At::Fill => "red",
-                                    }
-                                ],
-                            ]
-                        } else {
-                            vec![]
-                        }
-                    }),
-                    (0..(width/10)).map(|index| {
-                        let x = index*10;
-                        let y = height;
-                        let h = 10;
-                        let padding = 10;
-
-                        let center_dir = (height as i32/2 - y).clamp(-1, 1);
-                        let mut elems = Vec::with_capacity(2);
-                        elems.push(path![
+                    "Example SVG"
+                ],
+                points.windows(2).map(|window| {
+                    if let [(ax, ay), (bx, by)] = window {
+                        vec![
+                            path![
                                 attrs!{
-                                    At::D => format!("M {} {} V {}", x, y, y + center_dir * h)
+                                    At::D => format!("M {} {} L {} {}", ax, ay, bx, by)
                                     At::Stroke => "black"
                                 }
-                            ]);
-                        if index % 10 == 0 {
-                            elems.push(text![
+                            ],
+                            circle![
                                 attrs!{
-                                    At::X => x,
-                                    At::Y => y + center_dir * (h + padding),
-                                    At::FontFamily => "-apple-system, system-ui, BlinkMacSystemFont, Roboto",
-                                    At::DominantBaseline => "middle",
-                                    At::TextAnchor => "middle",
-                                    At::FontSize => "9",
-                                    At::Fill => "black",
-                                },
-                                index.to_string()
-                            ])
-                        }
-                        elems
-                    }),
-                    (0..(height/10)).map(|index| {
-                        let y = index*10;
-                        let x = 0;
-                        let l = width as i32;
-                        let xp = 10;
-                        let yp = 10;
-
-                        let center_dir = (width as i32/2 - x).clamp(-1, 1);
-                        let mut elems = Vec::with_capacity(2);
-                        let emphathize = index % 10 == 0;
-                        let opacity = if emphathize { 0.3 } else { 0.1 };
-                        elems.push(path![
-                                attrs!{
-                                    At::D => format!("M {} {} H {}", x, y, x + center_dir * l),
-                                    At::Stroke => "black",
-                                    At::Opacity => opacity,
+                                    At::Cx => format!("{}", bx)
+                                    At::Cy => format!("{}", by)
+                                    At::R => "1",
+                                    At::Fill => "red",
                                 }
-                            ]);
-                        if emphathize {
-                            elems.push(text![
-                                attrs!{
-                                    At::X => x + xp,
-                                    At::Y => y + yp,
-                                    At::FontFamily => "-apple-system, system-ui, BlinkMacSystemFont, Roboto",
-                                    At::DominantBaseline => "middle",
-                                    At::TextAnchor => "middle",
-                                    At::FontSize => "9",
-                                    At::Fill => "black",
-                                },
-                                ((height - y)/10).to_string()
-                            ])
-                        }
-                        elems
-                    })
-                ]
-            ]
+                            ],
+                        ]
+                    } else {
+                        vec![]
+                    }
+                }),
+                self.vertical_lines(),
+                self.horizontal_lines(),
+            ],
+        ]
+    }
+}
+// Graph SVG
+// viewport (history_length+pad, max_price+pad)
+// viewbox scalable
+//
+impl View for Model {
+    fn view(&self) -> Node<Self::Msg> {
+        div![
+            self.update_button(),
+            self.graph_view(),
         ]
     }
 }
