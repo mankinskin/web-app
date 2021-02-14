adc4be7e1005891e78eecfc4d187508a8900952f
Linus Behrbohm <linusbehrbohm@web.de>
2021-02-14 02:58:41 +0100

    Add time guides to timeline

--- morse-ui/client/src/timeline.rs
+++ morse-ui/client/src/timeline.rs
@@ -26,104 +26,81 @@ pub enum Msg {
     Rendered(RenderInfo),
 }
 pub struct Timeline {
-    times: Vec<(f64, f64)>,
-    line_x: f64,
-    ms_width: f64,
+    boxes: Vec<(f64, f64)>,
+    guides: Vec<f64>,
     started: Option<f64>,
     unit_time: Duration,
+    time_window: Duration,
     last_timestamp: Option<f64>,
-    units: f64,
+    box_height: u32,
+    width: u32,
+    height: u32,
 }
 pub fn timestamp_now() -> Option<f64> {
     Some(window().performance()?.now())
 }
 impl Timeline {
-    pub fn millis_to_units(&self, timestamp: f64) -> f64 {
-        timestamp/self.unit_time.as_millis() as f64
+    fn milli_width(&self) -> f64 {
+        self.width as f64/self.time_window.as_millis() as f64
     }
-    pub fn update_time(&mut self) {
-        let timestamp = timestamp_now().unwrap();
-        let delta = if let Some(prev) = self.last_timestamp.replace(timestamp) {
-            timestamp - prev
-        } else {
-            timestamp
-        };
-        self.units += self.millis_to_units(delta);
+    fn time_to_width(&self, time: f64) -> f64 {
+        self.milli_width() * time
     }
-    pub fn unit_width(&self) -> f64 {
-        self.ms_width * self.unit_time.as_millis() as f64
+    fn update_time(&mut self) {
+        self.last_timestamp.replace(timestamp_now().unwrap());
     }
-    pub fn visible_units(&self) -> f64 {
-        self.line_x/self.unit_width()
+    fn take_timestamp(&mut self) -> f64 {
+        if let Some(t) = self.last_timestamp {
+            return t;
+        }
+        let t = timestamp_now().unwrap();
+        self.last_timestamp = Some(t);
+        t
     }
-    pub fn remove_invisible(&mut self) {
-        let visible_units = self.visible_units();
-        let units_now = self.units;
-        self.times.retain(|(_, end)| *end >= units_now - visible_units);
+    fn remove_invisible(&mut self) {
+        let now = self.take_timestamp();
+        let window = self.time_window.as_millis() as f64;
+        self.boxes.retain(|(_, end)| now - *end <= window);
+        self.guides.retain(|t| now - *t <= window);
     }
     pub fn set_unit_time(&mut self, unit_time: Duration) {
         self.unit_time = unit_time;
         self.update_time();
     }
-}
-impl Init<Duration> for Timeline {
-    fn init(unit_time: Duration, orders: &mut impl Orders<Msg>) -> Self {
-        orders.after_next_render(Msg::Rendered);
-        Self {
-            times: Vec::new(),
-            line_x: 280.0,
-            ms_width: 0.05,
-            started: None,
-            unit_time,
-            units: 0.,
-            last_timestamp: None,
+    fn start(&mut self) {
+        let now = self.take_timestamp();
+        self.started = Some(now);
+    }
+    fn stop(&mut self) {
+        if let Some(start) = self.started.take() {
+            let now = self.take_timestamp();
+            self.boxes.push((start, now));
         }
     }
-}
-impl Component for Timeline {
-    type Msg = Msg;
-    fn update(&mut self, msg: Self::Msg, orders: &mut impl Orders<Self::Msg>) {
-        self.update_time();
-        match msg {
-            Self::Msg::Start => {
-                debug!("Start!");
-                self.started = Some(self.units);
-            },
-            Self::Msg::Stop => {
-                debug!("Stop!");
-                if let Some(start) = self.started.take() {
-                    let end = self.units;
-                    self.times.push((start, end));
-                }
-            },
-            Self::Msg::Rendered(_) => {
-                self.remove_invisible();
-                orders.after_next_render(Msg::Rendered);
-            }
+    fn boxes(&self) -> Vec<Node<Msg>> {
+        if let Some(now) = self.last_timestamp {
+            self.boxes.iter()
+                .map(|(begin, end)| {
+                    let (w, h) = (self.time_to_width(end - begin), self.box_height);
+                    let (x, y) = (self.width as f64 - self.time_to_width(now-begin), self.height/2);
+                    rect![
+                        attrs!{
+                            At::X => format!("{}px", x),
+                            At::Y => format!("{}px", y),
+                            At::Width => format!("{}px", w),
+                            At::Height => format!("{}px", h),
+                            At::Fill => "red",
+                        }
+                    ]
+                }).collect()
+        } else {
+            Vec::new()
         }
     }
-}
-impl Viewable for Timeline {
-    fn view(&self) -> Node<Msg> {
-        let (width, height) = (400, 100);
-        let box_height = 5;
-        let boxes: Vec<Node<Self::Msg>> = self.times.iter()
-            .map(|(begin, end)| {
-                let (w, h) = (self.unit_width() * (end - begin), box_height);
-                let (x, y) = (self.line_x - self.unit_width() * (self.units-begin), 50);
-                rect![
-                    attrs!{
-                        At::X => format!("{}px", x),
-                        At::Y => format!("{}px", y),
-                        At::Width => format!("{}px", w),
-                        At::Height => format!("{}px", h),
-                        At::Fill => "red",
-                    }
-                ]
-            }).collect();
-        let new_box: Node<Self::Msg> = if let Some(begin) = self.started {
-            let (w, h) = (self.unit_width() * (self.units-begin), box_height);
-            let (x, y) = (self.line_x - w, 50);
+    fn new_box(&self) -> Node<Msg> {
+        if let Some((now, begin)) = self.last_timestamp.zip(self.started) {
+            let (w, h) = (self.time_to_width(now-begin), self.box_height);
+            let (x, y) = (self.width as f64 - w, self.height/2);
             rect![
                 attrs!{
                     At::X => format!("{}px", x),
@@ -135,33 +112,91 @@ impl Viewable for Timeline {
             ]
         } else {
             empty![]
-        };
-        let fract = self.visible_units().fract();
-        let visible_units = self.visible_units().ceil() as u64;
-        let guides: Vec<Node<Self::Msg>> = (0..visible_units).map(|unit| {
-                    path![
-                        attrs!{
-                            At::D => format!("M{} 0 V{}", (self.line_x - fract) - unit as f64*self.unit_width() , height),
-                            At::Stroke => "grey",
-                        },
-                    ]
-                }).collect();
-        div![
-            p![format!("Boxes: {}", self.times.len())],
-            svg![
-                attrs!{
-                    At::Width => format!("{}px", width),
-                    At::Height => format!("{}px", height),
-                },
-                guides,
+        }
+    }
+    fn start_line(&self) -> Node<Msg> {
+        path![
+            attrs!{
+                At::D => format!("M{} 0 V{}", self.width, self.height),
+                At::Stroke => "black",
+            },
+        ]
+    }
+    fn guides(&self) -> Vec<Node<Msg>> {
+        if let Some(now) = self.last_timestamp {
+            self.guides.iter().map(|t| {
                 path![
                     attrs!{
-                        At::D => format!("M{} 0 V{}", self.line_x, height),
-                        At::Stroke => "black",
+                        At::D => format!("M{} 0 V{}", self.width as f64 - self.time_to_width(now - t), self.height),
+                        At::Stroke => "grey",
                     },
-                ],
-                boxes,
-                new_box,
+                ]
+            }).collect()
+        } else {
+            Vec::new()
+        }
+    }
+}
+impl Init<Duration> for Timeline {
+    fn init(unit_time: Duration, orders: &mut impl Orders<Msg>) -> Self {
+        orders.after_next_render(Msg::Rendered);
+        Self {
+            boxes: Vec::new(),
+            started: None,
+            unit_time,
+            time_window: Duration::from_secs(3),
+            last_timestamp: None,
+            box_height: 5,
+            width: 400, 
+            height: 100,
+            guides: Vec::new(),
+        }
+    }
+}
+impl Component for Timeline {
+    type Msg = Msg;
+    fn update(&mut self, msg: Self::Msg, orders: &mut impl Orders<Self::Msg>) {
+        self.update_time();
+        match msg {
+            Self::Msg::Start => {
+                //debug!("Start!");
+                self.start()
+            },
+            Self::Msg::Stop => {
+                //debug!("Stop!");
+                self.stop()
+            },
+            Self::Msg::Rendered(_) => {
+                self.remove_invisible();
+                orders.after_next_render(Msg::Rendered);
+            }
+        }
+        if let Some(start) = self.boxes.first().map(|(s, _)| *s).or(self.started) {
+            if let Some(last) = self.guides.last().cloned() {
+                let now = self.take_timestamp();
+                let unit_time = self.unit_time.as_millis() as f64;
+                let n = ((now - last)/unit_time).floor() as u32;
+                self.guides.extend((1..=n).map(|k| last + k as f64 * unit_time));
+            } else {
+                self.guides.push(start);
+            }
+        }
+    }
+}
+impl Viewable for Timeline {
+    fn view(&self) -> Node<Msg> {
+        div![
+            p![format!("Boxes: {}", self.boxes.len())],
+            p![format!("Guides: {}", self.guides.len())],
+            svg![
+                attrs!{
+                    At::Width => format!("{}px", self.width),
+                    At::Height => format!("{}px", self.height),
+                },
+                self.guides(),
+                self.start_line(),
+                self.boxes(),
+                self.new_box(),
                 ev(Ev::MouseDown, |_| {
                     Self::Msg::Start       
                 }),
