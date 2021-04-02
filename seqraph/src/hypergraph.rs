4c518e8404e3b0f488268aa6ba2c6020c398c11c
Linus Behrbohm <linusbehrbohm@web.de>
2021-04-02 16:57:30 +0200

    Add pattern prefix comparision

--- seqraph/src/hypergraph.rs
+++ seqraph/src/hypergraph.rs
@@ -22,37 +22,61 @@ use crate::{
 };
 use std::collections::HashSet;
 use indexmap::IndexSet;
+use std::borrow::Borrow;
 
 type VertexIndex = usize;
-type VertexParents = HashSet<Parent>;
-type VertexChildren = HashSet<Child>;
+type VertexParents = Vec<Parent>;
+type ChildPatterns = Vec<Pattern>;
+type Pattern = Vec<Child>;
 type TokenPosition = usize;
 type IndexPattern = Vec<VertexIndex>;
 type VertexPattern = Vec<VertexData>;
-type IndexPatternView<'a> = &'a[&'a VertexIndex];
+type PatternView<'a> = &'a[Child];
 type VertexPatternView<'a> = Vec<&'a VertexData>;
 type VertexPatternViewMut<'a> = Vec<&'a mut VertexData>;
 #[derive(Debug, Hash, PartialEq, Eq)]
-enum VertexKey<T: Tokenize> {
+pub enum VertexKey<T: Tokenize> {
     Token(Token<T>),
     Pattern(VertexIndex)
 }
-#[derive(Debug, Hash, PartialEq, Eq)]
-struct Parent {
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Parent {
     index: VertexIndex, // the parent pattern
-    positions: Vec<usize>, // positions of child in parent patterns
+    width: usize,
+    positions: HashSet<(usize, usize)>, // positions of child in parent patterns
+}
+impl Parent {
+    pub fn new(index: VertexIndex, width: usize) -> Self {
+        Self {
+            index,
+            width,
+            positions: Default::default(),
+        }
+    }
+    pub fn add_position(&mut self, index: usize, pos: usize) {
+        self.positions.insert((index, pos));
+    }
 }
 impl Into<VertexIndex> for Parent {
     fn into(self) -> VertexIndex {
         self.index
     }
 }
-#[derive(Debug, Hash, PartialEq, Eq)]
-struct Child {
-    pattern: IndexPattern
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Child {
+    index: VertexIndex, // the child index
+    width: usize, // the token width
 }
-#[derive(Debug, PartialEq, Eq)]
-struct VertexData {
+impl Child {
+    pub fn new(index: VertexIndex, width: usize) -> Self {
+        Self {
+            index,
+            width,
+        }
+    }
+}
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct VertexData {
     width: usize,
     family: VertexFamily
 }
@@ -64,16 +88,31 @@ impl VertexData {
         }
     }
 }
-#[derive(Debug, PartialEq, Eq)]
-struct VertexFamily {
-    children: VertexChildren,
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct VertexFamily {
     parents: VertexParents,
+    children: ChildPatterns,
 }
 impl VertexFamily {
     pub fn new() -> Self {
         Self {
             parents: VertexParents::new(),
-            children: VertexChildren::new(),
+            children: ChildPatterns::new(),
+        }
+    }
+    pub fn add_pattern<'c, I: IntoIterator<Item=&'c Child>>(&mut self, pat: I) {
+        // TODO: detect unmatching pattern
+        self.children.push(pat.into_iter().cloned().collect())
+    }
+    pub fn add_parent(&mut self, vertex: VertexIndex, width: usize, pattern_index: usize, position: usize) {
+        if let Some(parent) = self.parents
+            .iter_mut()
+            .find(|parent| parent.index == vertex) {
+            parent.add_position(pattern_index, position);
+        } else {
+            let mut parent = Parent::new(vertex, width);
+            parent.add_position(pattern_index, position);
+            self.parents.push(parent);
         }
     }
 }
@@ -85,6 +124,17 @@ impl std::default::Default for VertexFamily {
 #[derive(Debug)]
 pub struct Hypergraph<T: Tokenize> {
     graph: indexmap::IndexMap<VertexKey<T>, VertexData>,
+}
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum Remainder {
+    Left(Pattern),
+    Right(Pattern),
+}
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum MatchResult {
+    Remainder(Remainder),
+    Matching,
+    Mismatch,
 }
 
 impl<'t, 'a, T> Hypergraph<T>
@@ -105,23 +155,26 @@ where
     fn get_token_data_mut(&mut self, token: &Token<T>) -> Option<&mut VertexData> {
         self.graph.get_mut(&VertexKey::Token(*token))
     }
-    fn get_vertex_data(&self, index: VertexIndex) -> Option<&VertexData> {
+    fn get_vertex_data<I: Borrow<VertexIndex>>(&self, index: I) -> Option<&VertexData> {
         self.get_vertex(index).map(|(_, v)| v)
     }
-    fn expect_vertex_data(&self, index: VertexIndex) -> &VertexData {
+    fn expect_vertex_data<I: Borrow<VertexIndex>>(&self, index: I) -> &VertexData {
         self.get_vertex(index).map(|(_, v)| v).expect("Invalid index!")
     }
     fn get_vertex_data_mut(&mut self, index: VertexIndex) -> Option<&mut VertexData> {
         self.get_vertex_mut(index).map(|(_, v)| v)
     }
-    fn get_vertex(&self, index: VertexIndex) -> Option<(&VertexKey<T>, &VertexData)> {
-        self.graph.get_index(index)
+    fn get_vertex<I: Borrow<VertexIndex>>(&self, index: I) -> Option<(&VertexKey<T>, &VertexData)> {
+        self.graph.get_index(*index.borrow())
     }
     fn get_vertex_mut(&mut self, index: VertexIndex) -> Option<(&mut VertexKey<T>, &mut VertexData)> {
         self.graph.get_index_mut(index)
     }
-    fn insert_token(&mut self, token: Token<T>) -> VertexIndex {
-        self.graph.insert_full(VertexKey::Token(token), VertexData::with_width(1)).0
+    pub fn insert_token(&mut self, token: Token<T>) -> VertexIndex {
+        self.insert_vertex(VertexKey::Token(token), VertexData::with_width(1))
+    }
+    pub fn insert_vertex(&mut self, key: VertexKey<T>, data: VertexData) -> VertexIndex {
+        self.graph.insert_full(key, data).0
     }
     fn to_token_indices(&mut self, tokens: impl IntoIterator<Item=Token<T>>) -> IndexPattern {
         tokens.into_iter()
@@ -131,14 +184,14 @@ where
             )
             .collect()
     }
-    fn expect_vertices<I: Into<VertexIndex>>(&self, indices: impl Iterator<Item=I>) -> VertexPatternView<'_> {
+    fn expect_vertices<I: Borrow<VertexIndex>>(&self, indices: impl Iterator<Item=I>) -> VertexPatternView<'_> {
         indices
-            .map(move |index| self.expect_vertex_data(index.into()))
+            .map(move |index| self.expect_vertex_data(index))
             .collect()
     }
-    fn get_vertices<I: Into<VertexIndex>>(&self, indices: impl Iterator<Item=I>) -> Option<VertexPatternView<'_>> {
+    fn get_vertices<I: Borrow<VertexIndex>>(&self, indices: impl Iterator<Item=I>) -> Option<VertexPatternView<'_>> {
         indices
-            .map(move |index| self.get_vertex_data(index.into()))
+            .map(move |index| self.get_vertex_data(index))
             .collect()
     }
     fn get_token_indices(&mut self, tokens: impl Iterator<Item=&'t Token<T>>) -> IndexPattern {
@@ -150,34 +203,181 @@ where
         }
         v
     }
-    pub fn index_sequence<N: Into<T>, I: IntoIterator<Item = N>>(&mut self, seq: I) {
-        let seq = seq.into_iter();
-        let mut hyperedge: IndexPattern = Vec::with_capacity(seq.size_hint().0);
-        let mut parents: Option<VertexParents> = None;
-        let tokens = T::tokenize(seq);
-        let indices = self.to_token_indices(tokens);
-        let vertices = self.expect_vertices(indices.iter().cloned());
-        let len = indices.len();
-        for (pos_in_seq, (vertex_index, vertex)) in indices.iter().zip(vertices).enumerate() {
-            let mut parent_families: Vec<Vec<_>> = vertex.family.parents.iter()
-                .map(|parent| {
-                    let vertex = self.expect_vertex_data(parent.index.clone());
-                    parent.positions
-                          .iter()
-                          .zip(&vertex.family.children)
-                          .collect()
-                })
-                .collect();
-            for post_index in indices.iter().skip(pos_in_seq + 1) {
-                if parent_families.len() < 1 {
-                    break;
-                }
-                parent_families.retain(|parent_positions|
-                    parent_positions.iter()
-                        .any(|(pos, child)| child.pattern.get(*pos + post_index).map(|elem| elem == vertex_index).unwrap_or(false))
-                );
-            }
+    fn match_parent_prefix(&self, sub_pat: PatternView<'a>, parent: &Parent, post_sup_pat: PatternView<'a>) -> MatchResult {
+        // match successors in parents where a is at beginning
+        let vert = self.expect_vertex_data(parent.index);
+        let children = &vert.family.children;
+        // find pattern where sub is at beginning
+        // TODO: any heuristics to find the most efficient pattern to compare?
+        if let Some((pattern_index, _pos_sub)) = parent.positions
+            .iter()
+            .find(|(_pattern_index, pos_sub)| *pos_sub == 0) {
+            println!("matching remaining pattern with children");
+            self.match_prefix(
+                &sub_pat[1..],
+                &[&children[*pattern_index][1..], post_sup_pat].concat()
+            )
+        } else {
+            MatchResult::Mismatch
         }
     }
+    fn pattern_width(pat: PatternView<'a>) -> usize {
+        pat.into_iter().fold(0, |acc, child| acc + child.width)
+    }
+    fn match_prefix_impl(&self,
+            sub_pat: PatternView<'a>,
+            post_sup_pat: PatternView<'a>,
+            sup: VertexIndex,
+            sub: VertexIndex,
+            sup_width: usize,
+        ) -> MatchResult {
+        // search parent of sub
+        let sub_vert = self.expect_vertex_data(sub);
+        if sub_vert.family.parents.len() < 1 {
+            return MatchResult::Mismatch;
+        }
+        let parents = &sub_vert.family.parents;
+        // if parents contain sup parent, only try that one
+        let sup_parent = parents.iter()
+            .find(|Parent { index, positions, .. }|
+                  *index == sup && positions.iter().any(|(_, pos)| *pos == 0)
+            );
+        // try matching and get remainder or mismatch
+        if let Some(parent) = sup_parent {
+            println!("sup found in parents");
+            self.match_parent_prefix(&sub_pat, parent, post_sup_pat)
+        } else {
+            println!("matching available parents");
+            // search sup in parents
+            sub_vert.family.parents
+                .iter()
+                .filter(|parent| parent.width < sup_width)
+                .find_map(|parent| match self.match_parent_prefix(sub_pat, parent, post_sup_pat) {
+                    MatchResult::Mismatch => None,
+                    r @ _ => Some(r),
+                })
+                .unwrap_or(MatchResult::Mismatch)
+        }
+    }
+    fn match_prefix(&self, pattern_a: PatternView<'a>, pattern_b: PatternView<'a>) -> MatchResult {
+        if pattern_a.is_empty() && pattern_b.is_empty() {
+            return MatchResult::Matching;
+        } else if pattern_a.is_empty() {
+            println!("left empty");
+            return MatchResult::Remainder(Remainder::Left(pattern_b.iter().cloned().collect()));
+        } else if pattern_b.is_empty() {
+            println!("right empty");
+            return MatchResult::Remainder(Remainder::Right(pattern_a.iter().cloned().collect()));
+        }
+        if let Some((pos, (&Child {
+                index: index_a,
+                width: width_a,
+            },
+            &Child {
+                index: index_b,
+                width: width_b,
+            }))) = pattern_a.iter().zip(pattern_b).enumerate().skip_while(|(_, (a, b))| *a == *b).next() {
+            // Note: depending on sizes of a, b it may be differently efficient
+            // to search for children or parents, large patterns have less parents,
+            // small patterns have less children
+            // search larger in parents of smaller
+            if width_a == width_b {
+                // relatives can not have same sizes
+                MatchResult::Mismatch
+            } else if width_a > width_b {
+                println!("right sub");
+                self.match_prefix_impl(
+                    &pattern_b[pos..],
+                    &pattern_a[pos+1..],
+                    index_a,
+                    index_b,
+                    width_a,
+                    //width_b
+                )
+            } else {
+                println!("left sub");
+                self.match_prefix_impl(
+                    &pattern_a[pos..],
+                    &pattern_b[pos+1..],
+                    index_b,
+                    index_a,
+                    width_b,
+                    //width_a
+                )
+            }
+        } else {
+            MatchResult::Matching
+        }
+    }
+    //pub fn index_sequence<N: Into<T>, I: IntoIterator<Item = N>>(&mut self, seq: I) {
+    //    let seq = seq.into_iter();
+    //    let mut hyperedge: IndexPattern = Vec::with_capacity(seq.size_hint().0);
+    //    let mut parents: Option<VertexParents> = None;
+    //    let tokens = T::tokenize(seq);
+    //    let indices = self.to_token_indices(tokens);
+    //    let vertices = self.expect_vertices(indices.iter().cloned());
+    //    let len = indices.len();
+    //    for (pos_in_seq, (vertex_index, vertex)) in indices.iter().zip(vertices).enumerate() {
+    //        let mut parent_families: Vec<Vec<_>> = vertex.family.parents.iter()
+    //            .map(|parent| {
+    //                let vertex = self.expect_vertex_data(parent.index.clone());
+    //                parent.positions
+    //                      .iter()
+    //                      .zip(&vertex.family.children)
+    //                      .collect()
+    //            })
+    //            .collect();
+    //        for post_index in indices.iter().skip(pos_in_seq + 1) {
+    //            if parent_families.len() < 1 {
+    //                break;
+    //            }
+    //            parent_families.retain(|parent_positions|
+    //                parent_positions.iter()
+    //                    .any(|(pos, child)| child.get(*pos + post_index).map(|elem| elem == vertex_index).unwrap_or(false))
+    //            );
+    //        }
+    //    }
+    //}
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::token::*;
+    #[test]
+    fn match_simple() {
+        let mut g = Hypergraph::new();
+        let a = g.insert_token(Token::Element('a'));
+        let b = g.insert_token(Token::Element('b'));
+        let c = g.insert_token(Token::Element('c'));
+
+        let mut ab_data = VertexData::with_width(2);
+        ab_data.family.add_pattern(&[Child::new(a, 1), Child::new(b, 1)]);
+        let ab = g.insert_vertex(VertexKey::Pattern(0), ab_data);
+        g.get_vertex_data_mut(a).unwrap().family.add_parent(ab, 2, 0, 0);
+        g.get_vertex_data_mut(b).unwrap().family.add_parent(ab, 2, 0, 1);
+
+        let mut bc_data = VertexData::with_width(2);
+        bc_data.family.add_pattern(&[Child::new(b, 1), Child::new(c, 1)]);
+        let bc = g.insert_vertex(VertexKey::Pattern(1), bc_data);
+        g.get_vertex_data_mut(b).unwrap().family.add_parent(bc, 2, 0, 0);
+        g.get_vertex_data_mut(c).unwrap().family.add_parent(bc, 2, 0, 1);
+
+        let mut abc_data = VertexData::with_width(3);
+        let a_bc_pattern = &[Child::new(a, 1), Child::new(bc, 2)];
+        let ab_c_pattern = &[Child::new(ab, 2), Child::new(c, 1)];
+        abc_data.family.add_pattern(a_bc_pattern);
+        abc_data.family.add_pattern(ab_c_pattern);
+        let abc = g.insert_vertex(VertexKey::Pattern(2), abc_data);
+        g.get_vertex_data_mut(a).unwrap().family.add_parent(abc, 3, 0, 0);
+        g.get_vertex_data_mut(bc).unwrap().family.add_parent(abc, 3, 0, 1);
+        g.get_vertex_data_mut(ab).unwrap().family.add_parent(abc, 3, 1, 0);
+        g.get_vertex_data_mut(c).unwrap().family.add_parent(abc, 3, 1, 1);
+        let a_b_c_pattern = &[Child::new(a, 1), Child::new(b, 1), Child::new(c, 1)];
+        assert_eq!(g.match_prefix(a_bc_pattern, ab_c_pattern), MatchResult::Matching);
+        assert_eq!(g.match_prefix(ab_c_pattern, a_bc_pattern), MatchResult::Matching);
+        assert_eq!(g.match_prefix(a_bc_pattern, a_b_c_pattern), MatchResult::Matching);
+        assert_eq!(g.match_prefix(a_b_c_pattern, a_b_c_pattern), MatchResult::Matching);
+        assert_eq!(g.match_prefix(ab_c_pattern, a_b_c_pattern), MatchResult::Matching);
+    }
+}
