49c7fd078212d70536fe25164bf86cb6ed81ea62
Linus Behrbohm <linusbehrbohm@web.de>
2021-06-29 20:57:08 +0200

    Add egui_app

---
+++ egui_app/src/app.rs
@@ -0,0 +1,131 @@
+use eframe::{egui, epi};
+
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
+#[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
+pub struct TemplateApp {
+    // Example stuff:
+    label: String,
+
+    graph: Option<String>,
+    // this how you opt-out of serialization of a member
+    #[cfg_attr(feature = "persistence", serde(skip))]
+    value: f32,
+}
+
+impl Default for TemplateApp {
+    fn default() -> Self {
+        Self {
+            // Example stuff:
+            label: "Hello World!".to_owned(),
+            value: 2.7,
+            graph: None,
+        }
+    }
+}
+
+impl epi::App for TemplateApp {
+    fn name(&self) -> &str {
+        "egui template"
+    }
+
+    /// Called by the framework to load old app state (if any).
+    #[cfg(feature = "persistence")]
+    fn load(&mut self, storage: &dyn epi::Storage) {
+        *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
+    }
+
+    /// Called by the frame work to save state before shutdown.
+    #[cfg(feature = "persistence")]
+    fn save(&mut self, storage: &mut dyn epi::Storage) {
+        epi::set_value(storage, epi::APP_KEY, self);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+    fn update(&mut self, ctx: &egui::CtxRef, frame: &mut epi::Frame<'_>) {
+        let Self { label, value, graph } = self;
+
+        // Examples of how to create different panels and windows.
+        // Pick whichever suits you.
+        // Tip: a good default choice is to just keep the `CentralPanel`.
+        // For inspiration and more examples, go to https://emilk.github.io/egui
+
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+            egui::menu::bar(ui, |ui| {
+                egui::menu::menu(ui, "File", |ui| {
+                    if ui.button("Open...").clicked() {
+                        // open graph file
+                        let mut dialog = native_dialog::FileDialog::default();
+                        let res = std::env::current_dir();
+                        if let Ok(current_dir) = &res {
+                            dialog = dialog.set_location(current_dir);
+                        }
+                        match dialog.show_open_single_file() {
+                            Ok(Some(filepath)) => {
+                                let result = std::fs::read_to_string(filepath);
+                                match result {
+                                    Ok(content) => { *graph = Some(content); }
+                                    Err(err) =>
+                                        native_dialog::MessageDialog::default()
+                                            .set_text(&format!("{}", err))
+                                            .show_alert()
+                                            .unwrap(),
+                                }
+                            },
+                            Ok(None) => {},
+                            Err(err) => 
+                                native_dialog::MessageDialog::default()
+                                    .set_text(&format!("{}", err))
+                                    .show_alert()
+                                    .unwrap(),
+                        }
+                    } else if ui.button("Quit").clicked() {
+                        frame.quit();
+                    }
+                });
+            });
+        });
+
+        egui::SidePanel::left("side_panel").show(ctx, |ui| {
+            ui.heading("Side Panel");
+
+            ui.horizontal(|ui| {
+                ui.label("Write something: ");
+                ui.text_edit_singleline(label);
+            });
+
+            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
+            if ui.button("Increment").clicked() {
+                *value += 1.0;
+            }
+
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::Center), |ui| {
+                ui.add(
+                    egui::Hyperlink::new("https://github.com/emilk/egui/").text("powered by egui"),
+                );
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // The central panel the region left after adding TopPanel's and SidePanel's
+
+            if let Some(graph) = graph {
+                ui.code(graph.clone());   
+            } else {
+                ui.heading("No file loaded");   
+            }
+            egui::warn_if_debug_build(ui);
+        });
+
+        if false {
+            egui::Window::new("Window").show(ctx, |ui| {
+                ui.label("Windows can be moved by dragging them.");
+                ui.label("They are automatically sized based on contents.");
+                ui.label("You can turn on resizing and scrolling if you like.");
+                ui.label("You would normally chose either panels OR windows.");
+            });
+        }
+    }
+}
